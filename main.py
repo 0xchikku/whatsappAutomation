619bc0376689f6b282b2f5c3b8817816713df2fe
0xchikku <0xchikku@gmail.com>
2021-08-21 15:55:47 +0530

    Coded Updated

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ import pywhatkit
 def sendMessage(mobileNo, message, hour, minute):
     pywhatkit.sendwhatmsg(mobileNo, message, hour, minute, tab_close= True, wait_time= 15)
 
+
+# Getting Input from User
+# Message You Wanna Send
 message = input('Enter the message: ').strip()
+# Whatsapp Number
 mobileNo = input('Enter valid Whatsapp No with country code : ').strip()
 print('Enter time in 24hours format')
+# Time : Hour
 hour = int(input('Hours: ').strip())
+# Time : Minute
 minute = int(input('Minutes: ').strip())
 
 
+# Function Call
 sendMessage(mobileNo,message,hour,minute)
